daf232c6a8c46f3eff394d70df75076529dda742
rm <romtho@mail.ru>
2021-09-21 19:59:42 +0300

    Initial commit

---
+++ src/com/rm/jdbc/starter/JdbcRunner.java
@@ -0,0 +1,15 @@
+package com.rm.jdbc.starter;
+
+import com.rm.jdbc.starter.util.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class JdbcRunner {
+
+    public static void main(String[] args) throws SQLException {
+        try (Connection connection = ConnectionManager.open()) {
+            System.out.println(connection.getTransactionIsolation());
+        }
+    }
+}
