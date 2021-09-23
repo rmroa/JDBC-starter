6568c6b07dce1bc91926683f705ee557c7f7bba8
rm <romtho@mail.ru>
2021-09-23 18:48:46 +0300

    Added Blob

--- src/com/rm/jdbc/starter/JdbcRunner.java
+++ src/com/rm/jdbc/starter/JdbcRunner.java
@@ -1,16 +1,18 @@
 package com.rm.jdbc.starter;
 
+import com.rm.jdbc.starter.custom.Blob;
 import com.rm.jdbc.starter.custom.Flights;
 import com.rm.jdbc.starter.custom.GeneratedKeys;
 import com.rm.jdbc.starter.custom.MetaData;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class JdbcRunner {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, IOException {
 
         Flights flights = new Flights();
         flights.getFlightsBetween(LocalDate.of(2020, 1, 1).atStartOfDay(), LocalDateTime.now());
@@ -20,5 +22,8 @@ public class JdbcRunner {
 
         GeneratedKeys generatedKeys = new GeneratedKeys();
         generatedKeys.returnGeneratedKeys();
+
+        Blob blob = new Blob();
+        blob.getImage();
     }
 }
---
+++ src/com/rm/jdbc/starter/custom/Blob.java
@@ -0,0 +1,58 @@
+package com.rm.jdbc.starter.custom;
+
+import com.rm.jdbc.starter.util.ConnectionManager;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Blob {
+
+    public void getImage() throws SQLException, IOException {
+        String sql = "SELECT image " +
+                "FROM aircraft " +
+                "WHERE id = ?";
+
+        try (Connection connection = ConnectionManager.open();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, 1);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                byte[] image = resultSet.getBytes("image");
+                Files.write(Path.of("resources", "boeing777new.jpg"), image, StandardOpenOption.CREATE);
+            }
+        }
+    }
+
+    public void saveImage() throws SQLException, IOException {
+        String sql = "UPDATE aircraft " +
+                "SET image = ? " +
+                "WHERE id = 1";
+        try (Connection connection = ConnectionManager.open();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setBytes(1, Files.readAllBytes(Path.of("resources", "boeing777.jpg")));
+            preparedStatement.executeUpdate();
+        }
+    }
+
+//    private static void saveImage() throws SQLException, IOException {
+//        String sql = "UPDATE aircraft " +
+//                "SET image = ? " +
+//                "WHERE id = 1";
+//        try (Connection connection = ConnectionManager.open();
+//             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+//            connection.setAutoCommit(false);
+//            Blob blob = connection.createBlob();
+//            blob.setBytes(1, Files.readAllBytes(Path.of("resources", "boeing777.jpg")));
+//
+//            preparedStatement.setBlob(1, blob);
+//            preparedStatement.executeUpdate();
+//            connection.commit();
+//        }
+//    }
+}
